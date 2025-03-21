fef447864afc854cd321715e0883e1c8dde86b65
QIMoro <qi.moro@uva.es>
2025-03-21 11:25:11 +0100

    main

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     f=open("/home/prueba.txt", 'a')
-    x=datetime.datetime.now()
-    f.write("Ahora son ... ", x)
+    #x=datetime.datetime.now()
+    f.write("Ahora son ... ", "la hora")
     f.close()
     return "Hola mundo (v3.0)- 21 de marzo de 2025"
