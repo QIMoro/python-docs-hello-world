dc6038bdeb1d42583b8f3ac9fae03b2da221655f
QIMoro <qi.moro@uva.es>
2025-03-21 10:59:27 +0100

    main

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    f=open("prueba.txt", 'w')
-    f.write("Escribo algo")
+    f=open("prueba.txt", 'a')
+    x=datetime.datetime.now()
+    f.write("Ahora son ... ", x)
     f.close()
-    return "Hola mundo (v2.0)- 21 de marzo de 2025"
+    return "Hola mundo (v3.0)- 21 de marzo de 2025"
